d0a43562bcb68c1bc893119dae6eaad38c5fc8da
Allan Viray <allang.v.dev@gmail.com>
2022-09-10 17:32:05 -0500

    Edit text and correct grammar

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -4,18 +4,21 @@ const Directions = () => {
   // This will be a dropdown shown on the game's "title screen", along with the "Start" button.
   return (
     <div className="directionsContainer">
-      <h2>Directions</h2>
+      <h2>How to Play</h2>
       <ul>
         <li>Click the "Start" button to begin the game.</li>
         <li>
           An image of a dog will be displayed, along with a set of answer
-          choices that best describe the breed of the dog shown in the image.
+          choices that describe the breed of the dog shown in the image.
         </li>
         <li>
           Your goal is to select the correct breed of the dog that is shown.
         </li>
-        <li>Choose the best answer, and click on the submit button.</li>
-        <li>That's all! Let's see how well you know your dog!</li>
+        <li>
+          Select your answer from the available choices, and then click on the
+          "Submit" button.
+        </li>
+        <li>That's all! Let's see how well you know your dogs!</li>
       </ul>
     </div>
   );
