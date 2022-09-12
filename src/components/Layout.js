58c3cdf41b5a586fb4587b1b3a251068bdd69b51
Allan Viray <allang.v.dev@gmail.com>
2022-09-12 16:59:09 -0500

    Create initial Route component
    - This will determine which "screen" to show--either TitleScreen or
      GameScreen component.
    
    - Test if working by manually entering in the url.
    
    - For example, localhost:3000 or localhost:3000/ will show the
      TitleScreen component, while localhost:3000/gamescreen will show
      the GameScreen component.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import TitleScreen from './TitleScreen';
 import GameScreen from './GameScreen';
+import Route from './Route';
 
 const Layout = () => {
   return (
     <div className="AppWrapper">
-      <GameScreen />
+      <Route path="/">
+        <TitleScreen />
+      </Route>
+      <Route path="/game">
+        <GameScreen />
+      </Route>
     </div>
   );
 };
