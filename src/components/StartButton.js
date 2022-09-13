c2158e9544324b6985530f3081fbf2a8dc84da61
Allan Viray <allang.v.dev@gmail.com>
2022-09-13 18:25:51 -0500

    Create styled component to apply styles within StartButton

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledStartButton = styled.a`
+  display: inline-block;
+  margin-bottom: 1rem;
+  cursor: default;
+
+  span {
+    display: inline-block;
+    padding: 0.75rem 1rem;
+    color: #f3f3f3;
+    background-color: #f44b12;
+  }
+`;
 
 const StartButton = () => {
   return (
     <React.Fragment>
-      <a href="#">
+      <StyledStartButton href="#">
         <span>Start</span>
-      </a>
+      </StyledStartButton>
     </React.Fragment>
   );
 };
