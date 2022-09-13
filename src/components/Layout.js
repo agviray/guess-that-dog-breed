c2adc967a20655e3b2bfb8e86df1d8ccd79fbce7
Allan Viray <allang.v.dev@gmail.com>
2022-09-12 20:46:03 -0500

    Create and apply Wrapper styled component in Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import TitleScreen from './TitleScreen';
 import GameScreen from './GameScreen';
 import Route from './Route';
 
+const Wrapper = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+`;
+
 const Layout = () => {
   return (
-    <div className="AppWrapper">
+    <Wrapper>
       <Route path="/">
         <TitleScreen />
       </Route>
       <Route path="/game">
         <GameScreen />
       </Route>
-    </div>
+    </Wrapper>
   );
 };
 
