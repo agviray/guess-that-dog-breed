528b67a57fc526fdfbf6dce49b91bd55c97f5c6b
Allan Viray <allang.v.dev@gmail.com>
2022-09-13 19:39:52 -0500

    Add logic to Link onClick event handler
    - onClick uses the window object to change the current page's url
      without reloading the current page.
    
    - onClick will also dispatch a navigation event.

--- src/components/Link.js
+++ src/components/Link.js
@@ -9,6 +9,15 @@ const StyledLink = styled.a`
 const Link = ({ children, href }) => {
   const onClick = (event) => {
     event.preventDefault();
+    // Calling window.history.pushState(...) alone, will change the url, but \
+    // will not do a full page reload.
+    window.history.pushState({}, '', href);
+
+    // The following lines will dispatch a navigation event.
+    // We are dispatching a navigation event in order to tell other sibling Route
+    // components that the url changed.
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
