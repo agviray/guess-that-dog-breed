e462b0929b7e522908817ec02ece72bede6f8780
Allan Viray <allang.v.dev@gmail.com>
2022-09-14 16:11:00 -0500

    Create and add StyledSettingsToggler to the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Link from './Link';
+import GearIcon from '../assets/icons/gear-icon.svg';
+
+const StyledSettingsToggler = styled.div`
+  display: inline-block;
+  width: 30px;
+
+  img {
+    max-width: 100%;
+  }
+`;
 
 const Navbar = () => {
   return (
@@ -7,7 +18,9 @@ const Navbar = () => {
       <div className="navbar">
         <nav>
           <div className="logo">Guess That Dog Breed!</div>
-          <div className="settingsIcon">Gear Icon</div>
+          <StyledSettingsToggler>
+            <img src={GearIcon} alt="gear icon" />
+          </StyledSettingsToggler>
           <div className="settingsMenu">
             <ul>
               <li>Directions</li>
