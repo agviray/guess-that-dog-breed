817e4f629b9f1115db808397331e16bc431441ea
Allan Viray <allang.v.dev@gmail.com>
2022-09-13 19:52:51 -0500

    Implement useEffect in Route.js component
    - useEffect contains onLocationChange function that listens for
      navigation event that will be dispatched by Link's onClick function.

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    // Listens for 'navigation event' dispatched by Link component's onClick handler.
+    const onLocationChange = () => {
+      console.log('The location has changed!');
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
+
+  // Listens for "route changed"
   return window.location.pathname === path ? children : null;
 };
 
